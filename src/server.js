b3744a780f6d010b1ad7aecf189a64acad4fc484
Janis-H <janis_h.aguilar@yahoo.com>
2020-12-15 10:22:02 -0800

    removed unnecessary comments in server.js

--- src/server.js
+++ src/server.js
@@ -6,9 +6,6 @@ const PORT = 3000;
 
 app.use('/build', express.static(path.resolve(__dirname, 'build')));
 
-// app.use('/build', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../build/bundle.js')))
-
-// app.use('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../public/index.html')));
 app.use('/', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
