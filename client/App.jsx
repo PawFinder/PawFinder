2e0d5fa4f29681ccb0b34ae2f425c694ad8c47ca
Tania <tanya.o.lind@gmail.com>
2020-12-16 15:26:22 -0800

    search

--- client/App.jsx
+++ client/App.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import LandingPage from './components/landing-page/LandingPage';
 import Footer from './components/Footer';
-import SelectPage from './components/landing-page/Search.jsx';
+import DialogSelect from './components/landing-page/Search';
 
 const App = () => (
   <>
     <NavBar />
-    <SelectPage />
+    <DialogSelect />
     <LandingPage />
     {/* <MainPage /> */}
     <Footer />
