a88e06d6b0b85f2ae5c9b4015aca84868fcee9a4
Janis-H <janis_h.aguilar@yahoo.com>
2020-12-16 10:54:04 -0800

    implemented google login

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GoogleLogin from 'react-google-login';
 
 import { __NavBar } from '../styles/__NavBar';
 import { __Btn, __Logo } from '../styles/__Utils';
 import pawFinderLogo from '../icons/location.svg';
 
 const NavBar = () => {
+  const [login, setLogin] = useState(false);
+
+  const responseGoogle = (response) => {
+    console.log(response);
+    setLogin(true);
+  }
+
   return (  
     <__NavBar>
       <__Btn secondary>Favs</__Btn>
       <__Logo main src={pawFinderLogo}/>
-      <__Btn primary>Login</__Btn>
+      <GoogleLogin 
+        clientId="949269870683-s8cpdloouslu92ikabu07ch9fsde9kuo.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy="single_host_origin"
+        render={renderProps => (
+          <__Btn primary onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</__Btn>
+        )}
+      />
+      {/* <__Btn primary>Login</__Btn> */}
     </__NavBar>
   ) 
 }
